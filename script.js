82244092418dc61f4bd39c188831540a6fa95796
amimtiaz <amemtiaj19@gmail.com>
2024-02-17 10:38:41 +0600

    added all function

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 const generateRGBColor = () => {
-    Math.random()
+   const r = Math.floor(Math.random() * 255);
+   const g = Math.floor(Math.random() * 255);
+   const b = Math.floor(Math.random() * 255);
+
+   return `rgb(${r}, ${g}, ${b})`;
 }
+
+//console.log(generateRGBColor()); 
+
+const updateColor = () => {
+    const color = generateRGBColor();
+    const colorBox = document.getElementById('colorBox');
+    const colorCode = document.getElementById('colorCode');
+
+    colorBox.style.backgroundColor = color;
+    colorCode.innerText = color;
+
+    
+}
+
+const copyColorCode = () => {
+   const colorCode = document.getElementById('colorCode');
+   const inputTag = document.createElement('input');
+   document.body.appendChild(inputTag);
+   inputTag.value = colorCode.innerText;
+
+   inputTag.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputTag);
+    alert('color copied to clipboard')
+
+}
+
+document.getElementById('copyButton').addEventListener('click', () => {
+
+    copyColorCode();
+
+});
+
+function generateNewColorBtn() {
+    updateColor();
+}
+
+updateColor();
+
